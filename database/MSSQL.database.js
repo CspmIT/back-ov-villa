cbff8bc514582d426ac959ff281b3f440bb51507
agust1n <acomba@coopmorteros.coop>
2025-06-17 07:07:42 -0300

    fix: renamed file config

--- database/MSSQL.database.js
+++ database/MSSQL.database.js
@@ -1,41 +1,56 @@
 const { Sequelize } = require('sequelize')
-const configDb = require('../config/config.json')
+const configDb = require('../config/config')
 
-SequelizeMorteros = new Sequelize(configDb.coopm_villa.database, configDb.coopm_villa.username, configDb.coopm_villa.password, {
-	host: configDb.coopm_villa.host,
-	port: configDb.coopm_villa.port,
-	dialect: configDb.coopm_villa.dialect,
-	dialectOptions: {
-		options: {
-			encrypt: false,
-		},
-	},
-})
+SequelizeMorteros = new Sequelize(
+    configDb.coopm_villa.database,
+    configDb.coopm_villa.username,
+    configDb.coopm_villa.password,
+    {
+        host: configDb.coopm_villa.host,
+        port: configDb.coopm_villa.port,
+        dialect: configDb.coopm_villa.dialect,
+        dialectOptions: {
+            options: {
+                encrypt: false,
+            },
+        },
+    }
+)
 
-SequelizeVilla = new Sequelize(configDb.bd_villa.database, configDb.bd_villa.username, configDb.bd_villa.password, {
-	host: configDb.bd_villa.host,
-	port: configDb.bd_villa.port,
-	dialect: configDb.bd_villa.dialect,
-	dialectOptions: {
-		options: {
-			encrypt: false,
-		},
-	},
-})
+SequelizeVilla = new Sequelize(
+    configDb.bd_villa.database,
+    configDb.bd_villa.username,
+    configDb.bd_villa.password,
+    {
+        host: configDb.bd_villa.host,
+        port: configDb.bd_villa.port,
+        dialect: configDb.bd_villa.dialect,
+        dialectOptions: {
+            options: {
+                encrypt: false,
+            },
+        },
+    }
+)
 
-SequelizeCooptech = new Sequelize(configDb.cooptech.database, configDb.cooptech.username, configDb.cooptech.password, {
-	host: configDb.cooptech.host,
-	port: configDb.cooptech.port,
-	dialect: configDb.cooptech.dialect,
-	dialectOptions: {
-		options: {
-			encrypt: false,
-		},
-	},
-})
+SequelizeCooptech = new Sequelize(
+    configDb.cooptech.database,
+    configDb.cooptech.username,
+    configDb.cooptech.password,
+    {
+        host: configDb.cooptech.host,
+        port: configDb.cooptech.port,
+        dialect: configDb.cooptech.dialect,
+        dialectOptions: {
+            options: {
+                encrypt: false,
+            },
+        },
+    }
+)
 
 module.exports = {
-	SequelizeMorteros,
-	SequelizeVilla,
-	SequelizeCooptech
+    SequelizeMorteros,
+    SequelizeVilla,
+    SequelizeCooptech,
 }
