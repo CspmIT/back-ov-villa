d167d847d7cc7d78637a76e8cdd6c8478ab6eb67
julieta <julietasoledad11@hotmail.com>
2025-08-11 07:38:04 -0300

    fix: envio y estetica email

--- services/EmailServices.js
+++ services/EmailServices.js
@@ -30,31 +30,29 @@ async function sendEmail(data, url) {
 async function sendRecoverPass(name, email, url) {
 	try {
 		const html = PasswordRecovery({ name: name, link: url })
-		console.log(url)
-		return true
-		// axios
-		//     .post(
-		//         'https://api-dmds-morteros.planisys.net/v1/envio/send_one_inline/',
-		//         {
-		//             campana_id: 14,
-		//             email: true,
-		//             contacto: { email: email, nombre: '', apellido: '' },
-		//             html: html
-		//         },
-		//         {
-		//             headers: {
-		//                 'Content-Type': 'application/json',
-		//                 Authorization: 'e8ba65dc4d01ba6c'
-		//             }
-		//         }
-		//     )
-		//     .then(function (response) {
-		//         console.log(response)
-		//         return response;
-		//     })
-		//     .catch(function (error) {
-		//         console.log(error.response)
-		//     })
+		axios
+		.post(
+			'https://api-dmds-morteros.planisys.net/v1/envio/send_one_inline/',
+			{
+				campana_id: 14,
+				email: true,
+				contacto: { email: email, nombre: '', apellido: '' },
+				html: html,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'e8ba65dc4d01ba6c',
+				},
+			}
+		)
+		.then(function (response) {
+			console.log(response)
+			return response
+		})
+		.catch(function (error) {
+			console.log(error.response)
+		})
 	} catch (error) {
 		throw new Error('Hubo un error ene el envio del email')
 	}
